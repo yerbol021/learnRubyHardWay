040a5244f9f6d96dd9da970119de7e0b695b1645
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-28 23:31:33 -0700

    add more ex on ruby intro

---
+++ ex33.rb
@@ -0,0 +1,20 @@
+def number_generator(limit, increment)
+i = 0
+numbers = []
+
+while i < 6
+  puts "At the top i is #{i}"
+  numbers.push(i)
+
+  i += 1
+  puts "Numbers now: ", numbers
+  puts "At the bottom i is #{i}"
+end
+
+puts "The numbers: "
+
+# remember you can write this 2 other ways?
+numbers.each {|num| puts num }
+
+end
+number_generator(6,1)
