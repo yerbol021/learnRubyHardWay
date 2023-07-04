6183e92692eb9c813aec2439ad65a6ede296e2f1
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-07-04 13:35:30 -0700

    more practice

--- launchSchool/9ex.rb
+++ launchSchool/9ex.rb
@@ -11,3 +11,19 @@ puts p.name
 
 p.name = "New Name"
 puts p.name
+
+
+player 1: what does 5 plus 3 equal?
+>9
+Player 1: seriously? no!
+P:1 2/3 vs P2 3/3
+------New turn------
+player 2: what does 2 plus 6 equal?
+>8
+Player 1: Yes, you are correct.
+P:1 2/3 vs P2 3/3
+------New turn------
+player 1: what does 1 plus 1 equal?
+>9
+Player 1: seriously? no!
+P:1 1/3 vs P2 3/3
